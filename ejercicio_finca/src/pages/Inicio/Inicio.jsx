537e5029e2155269c4abc0e84c6270edc655e117
--unset <--unset>
2021-12-02 17:12:02 -0500

    primer commit del frontend a heroku

--- ejercicio_finca/src/pages/Inicio/Inicio.jsx
+++ ejercicio_finca/src/pages/Inicio/Inicio.jsx
@@ -31,7 +31,7 @@ const Inicio = () => {
             }
         })
         .catch((error) => {
-            console.log(error);
+            console.log(error); 
         });
     };
     useEffect(() => {
